8fa8a38a4267613298dcd609a8112d363377aed1
Aloïs Deniel <alois.deniel@gmail.com>
2016-01-22 00:08:01 +0100

    Initial version.

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # chain.lua
-Chaining updatable elements for lua.
+
+A tiny library for chaining updatable elements. It has been elaborated to be used in conjunction with [tween.lua](https://github.com/kikito/tween.lua) and [delay.lua](https://github.com/aloisdeniel/delay.lua) for creating complete animations or game cutscenes.
+
+# Example
+
+```lua
+local tween = require 'tween'
+local delay = require 'delay'
+local chain = require 'chain'
+
+local point = { x = 10, y = 10}
+local point2 = { x = 50, y = 50}
+
+local exampleChain = chain.new({
+	tween.new(3,point, {x=100}, 'inOutSine'),
+	delay.new(1),
+	tween.new(3,point, {y=100}, 'inOutSine'),
+	delay.new(1),
+	{ tween.new(3,point, {y=100}, 'inOutSine'), tween.new(1,point2, {x=100, y=100}, 'inOutSine') } -- In parralel
+})
+
+if exampleChain:update(dt) then
+	print("Finished !")
+end
+```
+
+# Installation
+
+Just copy the `chain.lua` file somewhere in your projects (maybe inside a `/lib/` folder) and require it accordingly.
+
+## Copyright and license
+
+MIT © [Aloïs Deniel](http://aloisdeniel.github.io)
---
+++ chain.lua
@@ -0,0 +1,74 @@
+local chain = {
+  _VERSION     = 'chain 1.0.0',
+  _DESCRIPTION = 'chaining updatable elements for lua',
+  _URL         = 'https://github.com/aloisdeniel/chain.lua',
+  _LICENSE     = [[
+    MIT LICENSE
+
+    Copyright (c) 2016 Alois Deniel
+
+    Permission is hereby granted, free of charge, to any person obtaining a
+    copy of this software and associated documentation files (the
+    "Software"), to deal in the Software without restriction, including
+    without limitation the rights to use, copy, modify, merge, publish,
+    distribute, sublicense, and/or sell copies of the Software, and to
+    permit persons to whom the Software is furnished to do so, subject to
+    the following conditions:
+
+    The above copyright notice and this permission notice shall be included
+    in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+    OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+    MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+    CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+    SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  ]]
+}
+
+-- Instance
+
+local Chain = {}
+local Chain_mt = {__index = Chain}
+
+function Chain:isFinished()
+  return self.current > #self.elements
+end
+
+function Chain:update(dt)
+  if self:isFinished() then return true end
+    
+  local currentFinished = true
+  local current = self.elements[self.current]
+  if type(current.update) == "function" then
+    currentFinished = current:update(dt)
+  else
+    for _,element in ipairs(self.current) do
+      currentFinished = element:update(dt) and currentFinished
+    end
+  end
+
+  if currentFinished then self.current = self.current + 1 end
+  
+  return self:isFinished()
+end
+
+function Chain:reset()
+  self.current = 1
+  for _,element in ipairs(self.elements) do
+    if element.clock > 0 then element:reset() end
+  end
+end
+
+-- Public interface
+
+function chain.new(elements)
+  return setmetatable({
+    elements = elements,
+    current = 1
+  }, Chain_mt)
+end
+
+return chain
